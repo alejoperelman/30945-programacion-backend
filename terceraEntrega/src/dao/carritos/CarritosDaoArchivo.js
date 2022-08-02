e4df2fdb6bff3e0e71ff609f9bd0eb281e620d53
Alejandro Perez <55850916+alejoperelman@users.noreply.github.com>
2022-08-02 10:59:52 -0300

    Tercera Entrega

---
+++ terceraEntrega/src/dao/carritos/CarritosDaoArchivo.js
@@ -0,0 +1,77 @@
+const { ContainerArchivo } = require('../../contenedores/ContainerArchivo');
+
+class Carritos extends ContainerArchivo {
+    constructor() {
+            super('./src/data/carritos.json');
+            //let carts = this.getAll();
+            //this.id = (carts.length > 0) ? carts.length + 1 : 1;
+        }
+        
+        async getAll() {
+            let carritos = await this.getContentFile();
+            return carritos;
+        }
+
+        async getById(id) {
+            let carritos = await this.getAll();            
+            let cart = null;
+            if(carritos.length > 0) {
+                let element = carritos.filter(elem => elem.id == parseInt(id));
+                if(element) {
+                    cart = element;
+                }
+            }
+            return cart;
+        }
+
+        async save() {
+            let carritos = await this.getAll();
+            let diaCart = Date();
+            let productos = [];
+            let sortedArray = carritos.sort((a, b)=> a.id - b.id);
+            let lastItem = sortedArray[sortedArray.length -1].id;
+            let lastId = parseInt(lastItem) + 1;
+            let carrito = {id:lastId, fecha: diaCart, productos: productos}
+            carritos.push(carrito);
+            this.saveInFile(carritos);
+        }
+
+        async addProdToCart(cartId, producto) {
+            let carritos = await this.getAll();
+            let carro = null;
+                if(carritos.length > 0) {
+                    let element = carritos.find(elem => elem.id == cartId);
+                        if(element) {
+                            element.productos.push(producto);
+                            carro = element;
+                            let delCarrito = await this.deleteById(cartId)
+                            carritos.push(carro);
+                            console.log(JSON.stringify(carritos));
+                            let sortedArray = carritos.sort((a, b)=> a.id - b.id);
+                            this.saveInFile(carritos);
+                        }
+
+                }
+                return carro;
+        }
+
+        async getProdById(id){
+            let carrito = await this.getById(id);
+            if(carrito.length > 0) {
+                return carrito.productos;
+            }
+        }
+
+        async deleteById(id) {
+            let carts = await this.getAll();
+            if(carts.length > 0) {
+                let elements = carts.filter((elem) => elem.id != id);
+                if(elements) {
+                    await this.saveInFile(elements);
+                }
+            }
+            return carts;
+        }
+}
+
+module.exports = {Carritos}
---
+++ terceraEntrega/src/models/productos.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const productosCollection = 'productos';
+
+const ProductosSchema = new mongoose.Schema({
+    nombre: {type: String, required: true, max: 100},
+    descripcion: {type: String, required: true, max: 200},
+    precio: {type: Number, required: true},
+    foto: {type: String, required: false, max: 100},
+    codigo: {type: String, required: true},
+    fecha: {type: Date, default: Date.now, required: false}
+})
+
+const productos = mongoose.model(productosCollection, ProductosSchema);
+
+module.export = productos
